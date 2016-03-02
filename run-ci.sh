c0b6e06fc8db8c12d5218ad6af601059c6edd228
Jared Allard <jaredallard@outlook.com>
2016-03-01 21:28:35 -0800

    run-ci: remove pushd/popd stdout, debug

--- run-ci.sh
+++ run-ci.sh
@@ -1,10 +1,15 @@
 #!/usr/bin/env bash
 
-echo 'ci@1.0.1'
+echo 'ci@1.0.2'
 
-pushd "/root/ci"
+pushd "/root/ci" 1>/dev/null
+
+echo "DEBUG: pulling git..."
 git pull
+
+echo "DEBUG: running node index.js"
 /usr/bin/env node index.js
-popd
+
+popd 1>/dev/null
 
 exit
